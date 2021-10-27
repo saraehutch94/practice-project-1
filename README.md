eadb6c213e3cc990b598e7d4b630d7e0dc03c29e
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-26 20:35:49 -0400

    added intiail files and content

---
+++ README.md
@@ -0,0 +1,24 @@
+# Random NASA
+
+A simple web app for displaying random images from NASA!
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript/jQuery
+- NASA API
+
+## Screenshots
+
+![screenshot 1](#)
+
+![screenshot 2](#)
+
+## Getting Started
+
+[Click here](#) to see the site live
+
+## Future Enhancements
+
+- Allow the user to see more details of a image by clicking on image card
---
+++ script.js
@@ -0,0 +1,18 @@
+// Constants
+const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+const API_KEY = 'Q12TTtYUnHgSTgAoxuKf2ibl9JgdCQf6lnM3qEZl';
+
+// State Variables
+let apiData;
+// Cached Element References
+const $main = $('main');
+// Event Listeners
+
+// Functions
+function getData() {
+    // get api data and assign it to our apiData state variable
+}
+
+function render() {
+    // transfer our api data to the DOM
+}
