5faf27a6887cb9dcb006d9aff16a154b7b0e9be2
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-26 21:38:40 -0400

    css style edits

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ const $main = $('main');
 // Event Listeners
 
 // Functions
+
+getData(); // call function immediately so that data loads on page load
+
 function getData() {
     // get api data and assign it to our apiData state variable
+    $.ajax(BASE_URL + '?api_key=' + API_KEY + '&count=6')
+    .then(function(data) {
+        apiData = data;
+        render();
+    }, function(error) {
+
+    });
 }
 
 function render() {
-    // transfer our api data to the DOM
+    const photoCards = apiData.map(function(photoObject) {
+        return`
+            <article style="background-image: url(${photoObject.url})">
+                <h3>${photoObject.title}</h3>
+            </article>`;
+    }).join(""); // .map transforms the elements in the array it was called on returning them to a new array
+    $main.html(`<section>${photoCards}</section>`);
 }
